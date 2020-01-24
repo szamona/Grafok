5fcbc2fd691f978f24119488f2088eff4c2f35a2
Szanto Monika <szantomomka@gmail.com>
2020-01-24 16:53:58 +0100

    javitva

--- src/hu/petrik/graffeladat/Graf.java
+++ src/hu/petrik/graffeladat/Graf.java
@@ -48,7 +48,7 @@ public class Graf {
         }
 
         // Ha már szerepel az él, akkor nem kell felvenni
-        for (El el: elek) {
+        for (El el : elek) {
             if (el.getCsucs1() == cs1 && el.getCsucs2() == cs2) {
                 return;
             }
@@ -58,16 +58,16 @@ public class Graf {
         elek.add(new El(cs2, cs1));
     }
 
-    public void SzelessegiBejar(int kezdopont) {
-        List<Integer> bejart = new LinkedList<>();
-        List<Integer> kovetkezok = new LinkedList<>();
+    public void szelessegiBejar(int kezdopont) {
+        List<Integer> bejart = new ArrayList<>();
+        Queue<Integer> kovetkezok = new LinkedList<>();
 
         kovetkezok.add(kezdopont);
         bejart.add(kezdopont);
 
         while (kovetkezok.size() > 0) {
-            var k = kovetkezok.get(0);
-            kovetkezok.remove(kovetkezok.get(0));
+            var k = kovetkezok.poll();
+//            kovetkezok.remove(kovetkezok.get(0));
             System.out.println(this.csucsok.get(k));
 
             for (var el : this.elek) {
@@ -79,21 +79,19 @@ public class Graf {
         }
     }
 
-    public void MelysegiBejar1(int kezdopont){
-        List<Integer> bejart = new LinkedList<>();
-        List<Integer> kovetkezok = new LinkedList<>();
+    public void melysegiBejar(int kezdopont) {
+        List<Integer> bejart = new ArrayList<>();
+        Stack<Integer> kovetkezok = new Stack<>();
 
         kovetkezok.add(kezdopont);
         bejart.add(kezdopont);
 
-        while (kovetkezok.size()>0){
-            var k=kovetkezok.get(kovetkezok.size());
-            kovetkezok.remove(kovetkezok.size());
+        while (kovetkezok.size() > 0) {
+            var k = kovetkezok.pop();
 
-            System.out.println(k);
+            System.out.println(this.csucsok.get(k));
 
-            for (var el : this.elek)
-            {
+            for (var el : this.elek) {
                 if (el.getCsucs1() == k && !bejart.contains(el.getCsucs2())) {
                     kovetkezok.add(el.getCsucs2());
                     bejart.add(el.getCsucs2());
@@ -103,18 +101,16 @@ public class Graf {
         }
     }
 
-    public boolean Osszefuggo(){
-        List<Integer> bejart = new LinkedList<>();
-        List<Integer> kovetkezok = new LinkedList<>();
+    public boolean osszefuggo() {
+        List<Integer> bejart = new ArrayList<>();
+        Queue<Integer> kovetkezok = new LinkedList<>();
 
         kovetkezok.add(0);
         bejart.add(0);
 
-        while (kovetkezok.size()>0){
-            var k = kovetkezok.get(0);
-            kovetkezok.remove(kovetkezok.get(0));
-            for (var el : this.elek)
-            {
+        while (kovetkezok.size() > 0) {
+            var k = kovetkezok.poll();
+            for (var el : this.elek) {
                 if (el.getCsucs1() == k && !bejart.contains(el.getCsucs2())) {
                     kovetkezok.add(el.getCsucs2());
                     bejart.add(el.getCsucs2());
@@ -122,32 +118,25 @@ public class Graf {
             }
         }
 
-        if(bejart.size()==this.csucsokSzama) {
-            return true;
-        }
-        else {
-            return false;
-        }
+        return bejart.size() == this.csucsokSzama;
     }
 
-    public Graf Feszitofa(){
+    public Graf feszitofa() {
         Graf fa = new Graf(this.csucsokSzama);
-        List<Integer> bejart = new LinkedList<>();
-        List<Integer> kovetkezok = new LinkedList<>();
+        List<Integer> bejart = new ArrayList<>();
+        Queue<Integer> kovetkezok = new LinkedList<>();
 
         kovetkezok.add(0);
         bejart.add(0);
 
-        while (kovetkezok.size()>0) {
-            var k = kovetkezok.get(0);
-            kovetkezok.remove(kovetkezok.get(0));
+        while (kovetkezok.size() > 0) {
+            var k = kovetkezok.poll();
 
-            for (var el : this.elek)
-            {
+            for (var el : this.elek) {
                 if (el.getCsucs1() == k && !bejart.contains(el.getCsucs2())) {
                     kovetkezok.add(el.getCsucs1());
                     bejart.add(el.getCsucs2());
-                    fa.hozzaad(el.getCsucs1(),el.getCsucs2());
+                    fa.hozzaad(el.getCsucs1(), el.getCsucs2());
                 }
             }
 
@@ -156,43 +145,46 @@ public class Graf {
 
     }
 
-    public HashMap Mohoszinezes(){
+    public HashMap mohoSzinezes() {
         var szinezes = new HashMap<Integer, Integer>();
-        var maxszín = this.csucsokSzama;
+        var maxSzin = this.csucsokSzama;
 
-        for (int i=0; i<this.csucsokSzama-1;i++){
-            List<Integer> valaszthatoSzinek=new LinkedList<>();
+        for (int i = 0; i < this.csucsokSzama; i++) {
+            List<Integer> valaszthatoSzinek = new ArrayList<>();
+            for (int j = 0; j < maxSzin; j++) {
+                valaszthatoSzinek.add(j);
+            }
 
-            for(var el : this.elek)
-            {
-                if(el.getCsucs1() == i && szinezes.containsKey(el.getCsucs2())){
+            for (var el : this.elek) {
+//                if (el.getCsucs1() == this.csucsok.get(i) && szinezes.containsKey(el.getCsucs2())) {
+                if (el.getCsucs1() == i && szinezes.containsKey(el.getCsucs2())) {
                     var szin = szinezes.get(el.getCsucs2());
                     valaszthatoSzinek.remove(szin);
                 }
             }
             var valasztottSzin = Collections.min(valaszthatoSzinek);
-            szinezes.put(i,valasztottSzin);
+            szinezes.put(i, valasztottSzin);
         }
         return szinezes;
     }
 
-    public HashMap Dijkstra(int egesz){
-        var csucsAdatok= new HashMap<Integer,CsucsAdat>();
-        for(int i=0; i<this.csucsokSzama-1;i++){
-            csucsAdatok.put(i,new CsucsAdat());
+    public HashMap dijkstra(int egesz) {
+        var csucsAdatok = new HashMap<Integer, CsucsAdat>();
+        for (int i = 0; i < this.csucsokSzama - 1; i++) {
+            csucsAdatok.put(i, new CsucsAdat());
             csucsAdatok.get(0).setKoltseg(0);
 
-            var vizsgaltDarab=0;
-            while (vizsgaltDarab<this.csucsokSzama){
+            var vizsgaltDarab = 0;
+            while (vizsgaltDarab < this.csucsokSzama) {
                 vizsgaltDarab++;
 
-                var vizsgaltCsucs= this.KovetkezoCsucs(csucsAdatok);
+                var vizsgaltCsucs = this.KovetkezoCsucs(csucsAdatok);
                 csucsAdatok.get(vizsgaltCsucs).setVizsgaltuk(true);
 
-                for(var el: this.elek){
-                    if(el.getCsucs1()==i){
-                        var ujKoltseg= csucsAdatok.get(vizsgaltCsucs).getKoltseg()+el.getSuly();
-                        if(ujKoltseg<csucsAdatok.get(el.getCsucs2()).getKoltseg()){
+                for (var el : this.elek) {
+                    if (el.getCsucs1() == i) {
+                        var ujKoltseg = csucsAdatok.get(vizsgaltCsucs).getKoltseg() + el.getSuly();
+                        if (ujKoltseg < csucsAdatok.get(el.getCsucs2()).getKoltseg()) {
                             csucsAdatok.get(el.getCsucs2()).setKoltseg(ujKoltseg);
                             csucsAdatok.get(el.getCsucs2()).setForrasCsucs(i);
                         }
@@ -204,23 +196,24 @@ public class Graf {
         return csucsAdatok;
     }
 
-    public int KovetkezoCsucs(HashMap<Integer,CsucsAdat> csucsAdatok){
-        var minIndex=0;
-        for ( var Index: csucsAdatok.keySet()){
-            if(csucsAdatok.get(Index).isVizsgaltuk()==false && csucsAdatok.get(Index).getKoltseg()<csucsAdatok.get(minIndex).getKoltseg()){
-                minIndex=Index;
+    public int KovetkezoCsucs(HashMap<Integer, CsucsAdat> csucsAdatok) {
+        var minIndex = 0;
+        for (var Index : csucsAdatok.keySet()) {
+            if (csucsAdatok.get(Index).isVizsgaltuk() == false && csucsAdatok.get(Index).getKoltseg() < csucsAdatok.get(minIndex).getKoltseg()) {
+                minIndex = Index;
             }
         }
         return minIndex;
     }
+
     @Override
     public String toString() {
         String str = "Csucsok:\n";
-        for (Csucs cs: csucsok) {
+        for (Csucs cs : csucsok) {
             str += cs + "\n";
         }
         str += "Elek:\n";
-        for (El el: elek) {
+        for (El el : elek) {
             str += el + "\n";
         }
         return str;
